f45346257c4c1f5b2f29abdeb0e17d6974bc9a45
hyiromori <hyiromori@gmail.com>
2021-01-19 17:18:12 +0900

    Generate UUID v4.

--- Rust/Wasmerを使ってみる/genuuid/src/main.rs
+++ Rust/Wasmerを使ってみる/genuuid/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use uuid::Uuid;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let uuid = Uuid::new_v4().to_hyphenated().to_string();
+    println!("{}", uuid);
+    Ok(())
 }
