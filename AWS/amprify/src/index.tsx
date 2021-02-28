5519b7af43b456c59ff72a9485eae64ba113c093
hyiromori <hyiromori@gmail.com>
2021-02-28 18:46:55 +0900

    Install Amplify libraries.
    https://docs.amplify.aws/start/getting-started/setup/q/integration/react#install-amplify-libraries

--- AWS/amprify/src/index.tsx
+++ AWS/amprify/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
