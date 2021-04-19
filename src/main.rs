5f49d8335cca0ae641e628dd8168bd8498d15465
mryhryki <mryhryki@gmail.com>
2021-04-19 22:58:14 +0900

    Devide file.

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,12 @@
+mod sentry_data;
 use std::env;
-use serde::{Deserialize};
+use crate::sentry_data::EventData;
 
 fn get_authorization_header_for_sentry() -> String {
     match env::var("SENTRY_AUTH_TOKEN") {
         Ok(val) => format!("Bearer {}", val),
         Err(_e) => "".to_string(),
     }
-}
-
-#[derive(Deserialize, Debug)]
-struct Tag {
-    key: String,
-    value: String,
-}
-
-#[derive(Deserialize, Debug)]
-struct UserData {
-    #[allow(non_snake_case)]
-    isStaff: Option<bool>,
-}
-
-#[derive(Deserialize, Debug)]
-struct User {
-    username: Option<String>,
-    name: Option<String>,
-    ip_address: Option<String>,
-    email: Option<String>,
-    data: Option<UserData>,
-    id: Option<String>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Event {
-    r#type: Option<String>,
-}
-
-// Ref: https://docs.sentry.io/api/events/list-an-issues-events/
-#[derive(Deserialize, Debug)]
-struct EventData {
-    #[allow(non_snake_case)]
-    eventID: String,
-    tags: Vec<Tag>,
-    #[allow(non_snake_case)]
-    dateCreated: String,
-    user: User,
-    message: String,
-    id: String,
-    platform: String,
-    event: Option<Event>,
-    #[allow(non_snake_case)]
-    groupID: String,
-    title: String,
 }
 
 #[tokio::main]
---
+++ src/sentry_data.rs
@@ -0,0 +1,46 @@
+use serde::{Deserialize};
+
+#[derive(Deserialize, Debug)]
+struct Tag {
+    key: String,
+    value: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct UserData {
+    #[allow(non_snake_case)]
+    isStaff: Option<bool>,
+}
+
+#[derive(Deserialize, Debug)]
+struct User {
+    username: Option<String>,
+    name: Option<String>,
+    ip_address: Option<String>,
+    email: Option<String>,
+    data: Option<UserData>,
+    id: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Event {
+    r#type: Option<String>,
+}
+
+// Ref: https://docs.sentry.io/api/events/list-an-issues-events/
+#[derive(Deserialize, Debug)]
+pub struct EventData {
+    #[allow(non_snake_case)]
+    eventID: String,
+    tags: Vec<Tag>,
+    #[allow(non_snake_case)]
+    dateCreated: String,
+    user: User,
+    message: String,
+    id: String,
+    platform: String,
+    event: Option<Event>,
+    #[allow(non_snake_case)]
+    groupID: String,
+    title: String,
+}
