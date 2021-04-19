fde58765879a235770150cf09e5b2df6ac760db6
mryhryki <mryhryki@gmail.com>
2021-04-19 22:53:06 +0900

    Define response type.

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,48 @@ fn get_authorization_header_for_sentry() -> String {
 }
 
 #[derive(Deserialize, Debug)]
-struct Data {
+struct Tag {
+    key: String,
+    value: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct UserData {
+    #[allow(non_snake_case)]
+    isStaff: Option<bool>,
+}
+
+#[derive(Deserialize, Debug)]
+struct User {
+    username: Option<String>,
+    name: Option<String>,
+    ip_address: Option<String>,
+    email: Option<String>,
+    data: Option<UserData>,
+    id: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Event {
+    r#type: Option<String>,
+}
+
+// Ref: https://docs.sentry.io/api/events/list-an-issues-events/
+#[derive(Deserialize, Debug)]
+struct EventData {
+    #[allow(non_snake_case)]
+    eventID: String,
+    tags: Vec<Tag>,
+    #[allow(non_snake_case)]
+    dateCreated: String,
+    user: User,
+    message: String,
     id: String,
+    platform: String,
+    event: Option<Event>,
+    #[allow(non_snake_case)]
+    groupID: String,
+    title: String,
 }
 
 #[tokio::main]
@@ -21,7 +61,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .header("Authorization", get_authorization_header_for_sentry())
         .send()
         .await?
-        .json::<Vec<Data>>()
+        .json::<Vec<EventData>>()
+        // .text()
         .await?;
     println!("{:?}", data);
     Ok(())
