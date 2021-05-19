516f12ffb9c3a88638418fcee2620d9a9551eff0
mryhryki <mryhryki@gmail.com>
2021-05-19 09:03:02 +0900

    Show URL list.

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,16 @@ fn get_authorization_header_for_sentry() -> String {
     }
 }
 
+fn copy_option(option: &Option<String>) -> Option<String> {
+    match option {
+        Some(val) => Some(val.clone()),
+        None => None,
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://sentry.io/api/0/issues/1197906478/events/";
+    let url = "https://sentry.io/api/0/issues/572307396/events/";
     let data = reqwest::Client::new()
         .get(url)
         .header("Authorization", get_authorization_header_for_sentry())
@@ -26,12 +33,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     println!();
-    let ip_address_list: Vec<Option<String>> = data.iter().map(|d| {
-        match &d.user.ip_address {
-            Some(val) => Some(val.clone()),
-            None => None,
-        }
-    }).collect();
+    let ip_address_list: Vec<Option<String>> = data
+        .iter()
+        .map(|d| copy_option(&d.user.ip_address))
+        .collect();
     show_summary_data(String::from("IP Addresses"), &ip_address_list);
 
     println!();
@@ -40,6 +45,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .map(|d| Some(String::from(&d.platform)))
         .collect();
     show_summary_data(String::from("Platform"), &platform_list);
+
+    println!();
+    let url_list: Vec<Option<String>> = data
+        .iter()
+        .map(|d| -> Option<String> {
+            match &d.tags.iter().find(|tag| tag.key == "url") {
+                Some(val) => Some(String::from(&val.value)),
+                None => None,
+            }
+        })
+        .collect();
+    show_summary_data(String::from("URL"), &url_list);
 
     println!();
     Ok(())
