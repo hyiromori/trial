26fa4c0c47b05b6074b9823a0b0e062ffd86605b
mryhryki <mryhryki@gmail.com>
2021-05-21 06:19:42 +0900

    get_title_by_url が正しく動いた！

--- rust/get_title_by_url/src/main.rs
+++ rust/get_title_by_url/src/main.rs
@@ -4,22 +4,15 @@ fn search_node(node: &Node, element_name: &String, nest: i32) -> Option<Node> {
     match node {
         Node::Element(element) => {
             if &element.name == element_name {
-                Some(Node::Element(element.clone()))
-            } else {
-                let match_node: Option<&Node> = element.children.iter().find(|child_node| {
-                    let match_node = search_node(&child_node, element_name, nest + 1);
-                    match match_node {
-                        Some(_) => true,
-                        None => false,
-                    }
-                });
-                let result = match match_node {
-                    Some(n) => Some(n.clone()),
-                    None => None,
-                };
-                println!("RESULT: {}, {:#?}", nest, result);
-                result
+                return Some(Node::Element(element.clone()));
             }
+            for child_node in element.children.iter() {
+                let matched_node = search_node(child_node, element_name, nest + 1);
+                if matched_node.is_some() {
+                    return matched_node;
+                }
+            }
+            None
         }
         _ => None,
     }
@@ -30,8 +23,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let html = reqwest::get("https://mryhryki.com/").await?.text().await?;
     let dom = Dom::parse(&html).unwrap();
     let node: &Node = dom.children.first().unwrap();
-    let _title_node: Option<Node> = search_node(node, &String::from("title"), 1);
+    let title_node: Option<Node> = search_node(node, &String::from("title"), 1);
 
-    // println!("TITLE_NODE: {:#?}", title_node);
+    println!("TITLE_NODE: {:#?}", title_node);
     Ok(())
 }
