605a781c94a9ac5dea1a74be27c5bc1700fccc58
mryhryki <mryhryki@gmail.com>
2021-05-28 18:08:00 +0900

    Generate random code.

--- rust/gen_rand/src/main.rs
+++ rust/gen_rand/src/main.rs
@@ -1,3 +1,15 @@
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+
+// https://rust-lang-nursery.github.io/rust-cookbook/algorithms/randomness.html
+fn random() -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Random: {}", random());
 }
