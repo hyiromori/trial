85424a839f4ae05b23b7a7307ef4f78062b87df5
mryhryki <mryhryki@gmail.com>
2021-05-30 08:19:29 +0900

    Success runs rust on lambda.

---
+++ rust/aws_lambda_example/deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE}")"; pwd)"
+cd "${THIS_DIR}"
+
+cargo build --release --target x86_64-unknown-linux-musl
+cp ./target/x86_64-unknown-linux-musl/release/aws_lambda_example ./bootstrap && zip lambda.zip bootstrap && rm bootstrap
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
+aws lambda create-function --function-name rust_example \
+  --handler doesnt.matter \
+  --zip-file fileb://./lambda.zip \
+  --runtime provided \
+  --role "arn:aws:iam::${AWS_ACCOUNT_ID}:role/DUMMY" \
+  --environment "Variables={RUST_BACKTRACE=1}" \
+  --tracing-config "Mode=Active"
+
+aws lambda invoke --function-name rust_example \
+  --payload '{"firstName": "world"}' \
+  output.json
